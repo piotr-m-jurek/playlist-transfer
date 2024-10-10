ecacc135398a1b9b689b7c078d31672064133184
Piotr Malecki-Jurek <piotr.m.jurek@checkhq.com>
2024-10-10 09:00:03 +0200

    add todo update types

---
+++ TODO.md
@@ -0,0 +1,8 @@
+* [spotify documentation]( https://developer.spotify.com/documentation/web-api/howtos/web-app-profile )
+
+- [json de/serializer yojson](https://github.com/ocaml-community/yojson)
+- [json de/serializer serde](https://github.com/serde-ml/serde)
+
+- [ ] get_access_token
+    - [ ] figure out how to add body to `post` request
+- [ ] getľ
--- bin/main.ml
+++ bin/main.ml
@@ -2,18 +2,7 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
-(* type _user_profile = *)
-(*   { country : string *)
-(*   ; display_name : string *)
-(*   ; email : string *)
-(*   ; _type : string *)
-(*   ; uri : string *)
-(*   ; href : string *)
-(*   ; id : string *)
-(*   ; product : string *)
-(*   } *)
-
-let body =
+let _body =
   Client.get (Uri.of_string "https://example.com")
   >>= fun (resp, body) ->
   let _ =
@@ -22,7 +11,54 @@ let body =
   body |> Cohttp_lwt.Body.to_string
 ;;
 
+(*
+   export async function getAccessToken(clientId: string, code: string): Promise<string> {
+    const verifier = localStorage.getItem("verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("code_verifier", verifier!);
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
+    });
+
+    const { access_token } = await result.json();
+    return access_token;
+}
+*)
+
+let get_access_token ~(client_id : string) ~client_secret =
+  Client.post
+    (Uri.of_string "https://accounts.spotify.com/api/token")
+    ~headers:(Header.init_with "Content-Type" "application/x-www-form-urlencoded")
+    ~body:
+      (Cohttp_lwt.Body.of_form
+         [ "client_id", [ client_id ]
+         ; "grant_type", [ "authorization_code" ]
+         ; "code", [ client_secret ]
+         ; "redirect_uri", [ "http://localhost:5173/callback" ]
+         ; "code_verifier", [ "verifier" (* TODO: WTH is verifier*) ]
+         ])
+  >>= fun (resp, body) ->
+  let _ =
+    Printf.printf "response code %d" (resp |> Response.status |> Code.code_of_status)
+  in
+  body |> Cohttp_lwt.Body.to_string
+;;
+
 let () =
-  let body = Lwt_main.run body in
-  print_endline ("Received body:\n" ^ body)
+  Dotenv.export () |> ignore;
+  let client_id = Sys.getenv "SPOTIFY_CLIENT_ID" in
+  let client_secret = Sys.getenv "SPOTIFY_CLIENT_SECRET" in
+  (* let body = Lwt_main.run body in *)
+  (* print_endline ("Received body:\n" ^ body); *)
+  print_endline "\n===SPOTIFY CLI===\n";
+  print_endline ("Client ID: \n" ^ client_id);
+  print_endline ("Client Secret: \n" ^ client_secret)
 ;;
